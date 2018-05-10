411b781ce7feb26e94d3ef62c04f3f7b7059ff71
Chris Walsh <cwalsh@leapfrogonline.com>
2018-05-10 13:46:11 -0500

    adds profiles

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -31,3 +31,7 @@ class Choice(models.Model):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=100, default='Please add a profile')
+
+    def __str__(self):
+        return self.user.username
